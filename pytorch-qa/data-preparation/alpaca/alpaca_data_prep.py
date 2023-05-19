943c4cc440a1fb4a30a759b5f5a44398d88783f4
Shrinath Suresh <shrinath@ideas2it.com>
2023-05-19 15:33:30 +0530

    Alpaca Data prep - update blogs and docs
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- pytorch-qa/data-preparation/alpaca/alpaca_data_prep.py
+++ pytorch-qa/data-preparation/alpaca/alpaca_data_prep.py
@@ -23,6 +23,7 @@ def load_and_clean_data(dataset_path):
 
     df = df[["question", "answer"]]
 
+    df = df.dropna().reset_index(drop=True)
     df["question"] = df["question"].apply(lambda x: cleanhtml(x))
     df["answer"] = df["answer"].apply(lambda x: cleanhtml(x))
     df["answer"] = df["answer"].str.replace("\n\n\n", "\n")
@@ -35,7 +36,7 @@ def load_and_clean_data(dataset_path):
     return df
 
 
-def generate_data_in_alpaca_format(df, max_length=2048, output_file_path="so_discuss_data.json"):
+def generate_data_in_alpaca_format(df, max_length=2048, output_file_path="final_data.json"):
     data = []
 
     for index, row in tqdm(df.iterrows()):
@@ -55,14 +56,20 @@ def generate_data_in_alpaca_format(df, max_length=2048, output_file_path="so_dis
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--stack_overflow_dataset_path", type=str, default="stack_overflow.json")
-    parser.add_argument("--pt_discuss_discuss_path", type=str, default="discussion_forum.json")
+    parser.add_argument("--pt_discuss_dataset_path", type=str, default="discussion_forum.json")
+    parser.add_argument("--pt_docs_dataset_path", type=str, default="pt_docs.json")
+    parser.add_argument("--pt_blogs_discuss_path", type=str, default="pt_blogs.json")
     args = parser.parse_args()
     so_df = load_and_clean_data(args.stack_overflow_dataset_path)
     print("SO Dataset: ", so_df.shape)
-    discuss_df = load_and_clean_data(args.pt_discuss_discuss_path)
+    discuss_df = load_and_clean_data(args.pt_discuss_dataset_path)
     print("Discuss Dataset: ", discuss_df.shape)
+    docs_df = load_and_clean_data(args.pt_docs_dataset_path)
+    print("Docs Dataset: ", docs_df.shape)
+    blogs_df = load_and_clean_data(args.pt_blogs_discuss_path)
+    print("Blogs Dataset: ", blogs_df.shape)
 
-    final_df = so_df.append(discuss_df, ignore_index=True)
+    final_df = pd.concat([so_df, discuss_df, docs_df, blogs_df])
     print("Merged Dataset: ", final_df.shape)
 
     generate_data_in_alpaca_format(df=final_df)
