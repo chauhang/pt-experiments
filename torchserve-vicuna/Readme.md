827eb2840fa92d222a826f9b4213d65138fd3c9f
Jagadeesh J <jagadeeshj@ideas2it.com>
2023-06-16 01:21:24 +0530

    fix vicuna TS serving
    Signed-off-by: Jagadeesh J <jagadeeshj@ideas2it.com>

--- torchserve-vicuna/Readme.md
+++ torchserve-vicuna/Readme.md
@@ -1,1 +1,18 @@
 # Loading vicuna-13b with PiPPy (PyTorch Native Large inference solution)
+
+
+## Download the model
+
+```bash
+```
+## Packaging the model
+
+```bash
+torch-model-archiver --model-name bloom --version 1.0 --handler custom_handler.py --extra-files  -r requirements.txt --config-file model-config.yaml --archive-format tgz
+```
+
+## Inference
+
+```bash
+curl -v "http://localhost:8080/predictions/vicuna-13b" -F "data=what is pytorch ?"
+```
--- torchserve-vicuna/custom_handler.py
+++ torchserve-vicuna/custom_handler.py
@@ -8,29 +8,33 @@ import torch
 import transformers
 from transformers import LlamaTokenizer, LlamaForCausalLM
 
-from ts.torch_handler.distributed.base_pippy_handler import BasePippyHandler
-from ts.handler_utils.distributed.pt_pippy import initialize_rpc_workers, get_pipeline_driver
+from ts.torch_handler.base_handler import BaseHandler
 
 logger = logging.getLogger(__name__)
 logger.info("Transformers version %s", transformers.__version__)
-
 if packaging.version.parse(torch.__version__) >= packaging.version.parse("2.0.0"):
     logger.info("PyTorch version is 2.0.0 or greater")
 else:
     logger.info(
         "PyTorch version is less than 2.0.0, initializing with meta device needs PyTorch 2.0.0 and greater"
     )
-class ModelHandler(BasePippyHandler, ABC):
+
+class ModelHandler(BaseHandler, ABC):
     def __init__(self):
         super(ModelHandler, self).__init__()
         self.initialized = False
 
     def initialize(self, ctx):
-        super().initialize(ctx)
         self.manifest = ctx.manifest
         properties = ctx.system_properties
         model_dir = properties.get("model_dir")
-        self.device = self.local_rank %  torch.cuda.device_count()
+
+        if torch.cuda.is_available():
+            self.map_location = "cuda"
+            self.device = torch.device(self.map_location)
+        else:
+            self.map_location = "cpu"
+            self.device = torch.device(self.map_location)
 
         model_path = ctx.model_yaml_config["handler"]["model_path"]
         seed = ctx.model_yaml_config["handler"]["manual_seed"]
@@ -41,34 +45,24 @@ class ModelHandler(BasePippyHandler, ABC):
 
         dtype = dtypes.get(dtype_str, torch.float32)
 
-        skip_init_start = time.perf_counter()
-        with torch.device("meta"):
-            self.model = LlamaForCausalLM.from_pretrained(
-                model_path, use_cache=False, torch_dtype=dtype
-            )
-        skip_init_end = time.perf_counter()
-        
-        logger.info(
-            f" init model time on meta device took {skip_init_end - skip_init_start} seconds"
+        self.model = LlamaForCausalLM.from_pretrained(
+            model_path, 
+            use_cache=False, 
+            max_memory={i: ctx.model_yaml_config["handler"]["max_gpu_memory"] for i in range(ctx.model_yaml_config["handler"]["num_gpus"])},
+            low_cpu_mem_usage=ctx.model_yaml_config["handler"]["low_cpu_mem_usage"],
+            device_map=ctx.model_yaml_config["handler"]["device_map"],
+            torch_dtype=dtype
         )
+
         self.tokenizer = LlamaTokenizer.from_pretrained(model_path, use_fast=False, return_tensors="pt")
-        self.tokenizer.pad_token = self.tokenizer.eos_token
 
         self.max_length = ctx.model_yaml_config["handler"]["max_length"]
         self.max_new_tokens = ctx.model_yaml_config["handler"]["max_new_tokens"]
 
-        logger.info("Instantiating model Pipeline")
-        pippy_compile_time_start = time.perf_counter()
-        self.model = get_pipeline_driver(self.model, self.world_size, ctx)
-        pippy_compile_time_end = time.perf_counter()
-
-        logger.info(
-            f" pippy compile time took {pippy_compile_time_end- pippy_compile_time_start} seconds on rank {self.local_rank}"
-        )
-
         logger.info("Transformer model from path %s loaded successfully", model_dir)
 
         self.initialized = True
+
     def preprocess(self, requests):
         """Basic text preprocessing, based on the user's chocie of application mode.
         Args:
@@ -77,8 +71,6 @@ class ModelHandler(BasePippyHandler, ABC):
         Returns:
             list : The preprocess function returns a list of Tensor for the size of the word tokens.
         """
-        input_ids_batch = None
-        attention_mask_batch = None
         for idx, data in enumerate(requests):
             input_text = data.get("data")
             if input_text is None:
@@ -86,33 +78,16 @@ class ModelHandler(BasePippyHandler, ABC):
             if isinstance(input_text, (bytes, bytearray)):
                 input_text = input_text.decode("utf-8")
 
-            max_length = self.setup_config["max_length"]
-            logger.info("Received text: '%s'", input_text)
+            logger.info("Received text: %s", input_text)
 
-            inputs = self.tokenizer.encode_plus(
-                input_text,
-                max_length=int(max_length),
-                pad_to_max_length=True,
-                add_special_tokens=True,
+            inputs = self.tokenizer(
+                [input_text],
+                max_length=self.max_length,
                 return_tensors="pt",
             )
+        return inputs
 
-            input_ids = inputs["input_ids"].to(self.device)
-            attention_mask = inputs["attention_mask"].to(self.device)
-            # making a batch out of the recieved requests
-            # attention masks are passed for cases where input tokens are padded.
-            if input_ids.shape is not None:
-                if input_ids_batch is None:
-                    input_ids_batch = input_ids
-                    attention_mask_batch = attention_mask
-                else:
-                    input_ids_batch = torch.cat((input_ids_batch, input_ids), 0)
-                    attention_mask_batch = torch.cat(
-                        (attention_mask_batch, attention_mask), 0
-                    )
-        return (input_ids_batch, attention_mask_batch)
-
-    def inference(self, input_batch):
+    def inference(self, inputs):
         """Predict the class (or classes) of the received text using the
         serialized transformers checkpoint.
         Args:
@@ -120,22 +95,16 @@ class ModelHandler(BasePippyHandler, ABC):
         Returns:
             list : It returns a list of the predicted value for the input text
         """
-        (input_ids_batch, _) = input_batch
         inferences = []
-        input_ids_batch = input_ids_batch.to(self.device)
+        inputs = inputs.to(self.device)
         outputs = self.model.generate(
-            input_ids_batch,
+            **inputs,
             do_sample=True,
-            max_new_tokens=int(self.setup_config["max_length"]),
-            top_p=0.95,
-            top_k=60,
+            max_new_tokens=self.max_new_tokens,
         )
-        for i, _ in enumerate(outputs):
-            inferences.append(
-                self.tokenizer.decode(outputs[i], skip_special_tokens=True)
-            )
+        inferences.append(self.tokenizer.decode(outputs[0], skip_special_tokens=True))
 
-        logger.info("Generated text: '%s'", inferences)
+        logger.info("Generated text: %s", inferences)
 
         print("Generated text", inferences)
         return inferences
