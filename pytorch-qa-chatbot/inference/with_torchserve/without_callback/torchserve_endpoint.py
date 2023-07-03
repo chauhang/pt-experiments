00104909ca069a8b858f9138798257cf9068d2a3
Jagadeesh J <jagadeeshj@ideas2it.com>
2023-07-03 22:25:25 +0530

    add REST stream response
    Signed-off-by: Jagadeesh J <jagadeeshj@ideas2it.com>

--- pytorch-qa-chatbot/inference/with_torchserve/without_callback/torchserve_endpoint.py
+++ pytorch-qa-chatbot/inference/with_torchserve/without_callback/torchserve_endpoint.py
@@ -1,12 +1,18 @@
 from typing import Any, Dict, List, Mapping, Optional
 from pydantic import Extra, root_validator
 from langchain.llms.base import LLM
-from langchain.callbacks.manager import CallbackManagerForLLMRun, AsyncCallbackManagerForLLMRun, CallbackManagerForChainRun
+from langchain.callbacks.manager import (
+    CallbackManagerForLLMRun,
+    AsyncCallbackManagerForLLMRun,
+    CallbackManagerForChainRun,
+)
 
-VALID_PROTOCOLS = ("gRPC")
+VALID_PROTOCOLS = ("REST", "gRPC")
+
 
 class TorchServeEndpoint(LLM):
     """Host Address to use."""
+
     host: str = "localhost"
     """Port to use."""
     port: str = "7070"
@@ -16,52 +22,122 @@ class TorchServeEndpoint(LLM):
     model_kwargs: Optional[dict] = None
     """Streaming"""
     streaming: bool = True
+    """Protocol"""
+    protocol: str = "gRPC"
     """Verbose"""
     verbose: bool = False
     """Response Object"""
     response: Any = None
     """Client Object"""
-    client: Any
+    client: Any = None
 
     @root_validator()
     def validate_environment(cls, values: Dict) -> Dict:
+        URL = values["host"] + ":" + values["port"]
         try:
-            import grpc
-            import inference_pb2_grpc
+            if values["protocol"] == "REST":
+                import requests
+            elif values["protocol"] == "gRPC":
+                import grpc
+                import inference_pb2_grpc
+            else:
+                raise ValueError(
+                    f'Got invalid protocol {values["protocol"]}, '
+                    f"currently only {VALID_PROTOCOLS} are supported"
+                )
+        except ImportError as e:
+            if "requests" in str(e):
+                raise ImportError(
+                    "Could not import requests."
+                    "Please install it with `pip install -U requests`."
+                )
+            else:
+                raise ImportError(
+                    "Could not import grpc proto buffers. "
+                    "Please install it with `pip install -U grpcio protobuf grpcio-tools`."
+                )
 
-            GPRC_URL = values["host"]+":"+values["port"]
-            channel = grpc.insecure_channel(GPRC_URL)
-            values["client"] = inference_pb2_grpc.InferenceAPIsServiceStub(channel)
-        except ImportError:
-            raise ImportError(
-                "Could not import grpc proto buffers. "
-                "Please install it with `pip install -U grpcio protobuf grpcio-tools`."
+        if values["protocol"] == "gRPC":
+            channel = grpc.insecure_channel(URL)
+            values["client"] = inference_pb2_grpc.InferenceAPIsServiceStub(
+                channel
             )
+
         return values
-    
-    def _infer_stream(self, stub, model_input):
+
+    def _grpc_infer_stream(self, stub, model_input):
         import inference_pb2
 
-        print("Model Name: ", self.model_name, "Input: ", model_input)
-        input_data = {"data": bytes(model_input, 'utf-8')}
-
         response = stub.StreamPredictions(
-            inference_pb2.PredictionsRequest(model_name=self.model_name, input=input_data)
+            inference_pb2.PredictionsRequest(
+                model_name=self.model_name, input=model_input
+            )
         )
         return response
 
-    def _call(self, prompt, run_manager: Optional[CallbackManagerForLLMRun] = None, stop=None, **kwargs: Any) -> str:
-        self.response = self._infer_stream(self.client, prompt)
-        return ""
+    def _rest_infer_stream(self, host, port, model_input):
+        import requests
 
-    async def _acall(self, prompt, run_manager: Optional[AsyncCallbackManagerForLLMRun] = None, stop=None, **kwargs: Any) -> str:
+        response = requests.post(
+            host + ":" + port + f"/predictions/{self.model_name}",
+            data=model_input,
+            stream=True,
+        )
+        return response
+
+    # TODO: Implement unary response for both REST and gRPC
+    def _call(
+        self,
+        prompt,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        stop=None,
+        **kwargs: Any,
+    ) -> str:
+        print("Model Name: ", self.model_name, "Input: ", prompt)
+
+        input_data = {"data": bytes(prompt, "utf-8")}
+
+        if self.streaming:
+            self.response = (
+                self._rest_infer_stream(
+                    self.host, self.port, input_data
+                ).iter_content(chunk_size=None)
+                if self.protocol == "REST"
+                else self._grpc_infer_stream(self.client, input_data)
+            )
+            return ""
+
+    # TODO: Implement unary response for both REST and gRPC
+    async def _acall(
+        self,
+        prompt,
+        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+        stop=None,
+        **kwargs: Any,
+    ) -> str:
+        print("Model Name: ", self.model_name, "Input: ", prompt)
+
+        input_data = {"data": bytes(prompt, "utf-8")}
+
         if self.streaming:
             combined_text_output = ""
-            self.response = self._infer_stream(self.client, prompt)
+            self.response = (
+                self._rest_infer_stream(
+                    self.host, self.port, input_data
+                ).iter_content(chunk_size=None)
+                if self.protocol == "REST"
+                else self._grpc_infer_stream(self.client, input_data)
+            )
             for resp in self.response:
-                prediction = resp.prediction.decode("utf-8")
+                prediction = (
+                    resp.decode("utf-8")
+                    if self.protocol == "REST"
+                    else resp.prediction.decode("utf-8")
+                )
                 if run_manager:
-                    await run_manager.on_llm_new_token(token=prediction, verbose=self.verbose)
+                    await run_manager.on_llm_new_token(
+                        token=prediction, verbose=self.verbose
+                    )
                 combined_text_output += prediction
             return combined_text_output
 
@@ -70,12 +146,16 @@ class TorchServeEndpoint(LLM):
         """Get the identifying parameters."""
         _model_kwargs = self.model_kwargs or {}
         return {
-            **{"host": self.host, 
-               "port": self.port, 
-               "model_name": self.model_name
-               },
+            **{
+                "host": self.host,
+                "port": self.port,
+                "model_name": self.model_name,
+                "protocol": self.protocol,
+                "streaming": self.streaming,
+            },
             **{"model_kwargs": _model_kwargs},
         }
+
     @property
     def _llm_type(self) -> str:
         return "torchserve_endpoint"
