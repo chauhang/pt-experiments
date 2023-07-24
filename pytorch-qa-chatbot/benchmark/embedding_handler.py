d0201b24d5f1d11add3d57d9493410504c18c27f
Shrinath Suresh <shrinath@ideas2it.com>
2023-07-24 11:00:55 +0530

    Adding batch support to embedding handler
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- pytorch-qa-chatbot/benchmark/embedding_handler.py
+++ pytorch-qa-chatbot/benchmark/embedding_handler.py
@@ -1,26 +1,40 @@
 import logging
+
+import torch
 from InstructorEmbedding import INSTRUCTOR
 from ts.torch_handler.base_handler import BaseHandler
 
 logger = logging.getLogger(__name__)
 
 
-class ModelHandler(BaseHandler):
+class EmbeddingModelHandler(BaseHandler):
     def __init__(self):
-        super(ModelHandler, self).__init__()
+        super(EmbeddingModelHandler, self).__init__()
         self.initialized = False
         self.model = None
+        self.device = "cuda:0"
 
     def initialize(self, ctx):
         model_name = ctx.model_yaml_config["handler"]["model_name"]
         self.model = INSTRUCTOR(model_name)
-        # Forcing to load on the first gpu
-        self.model.to("cuda:0")
+        self.model.to(self.device)
 
     def preprocess(self, data):
-        row = data[0]["data"]
-        row = row.decode("utf-8")
-        return [row]
+        sentences = []
+        for row in data:
+            row = row["data"]
+            row = row.decode("utf-8")
+            sentences.append(row)
+
+        return sentences
 
     def inference(self, data):
-        return self.model.encode(data)
+
+        embeddings = []
+        for row in data:
+            embedding = self.model.encode(row)
+            print("Embedding: ", embedding.size)
+            embedding = torch.from_numpy(embedding).to(self.device)
+            embeddings.append(embedding)
+
+        return torch.stack(embeddings)
